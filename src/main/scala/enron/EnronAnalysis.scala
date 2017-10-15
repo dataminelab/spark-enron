832bc731d928dfc6a6230d9389065192897c73a7
rathko <radek@dataminelab.com>
2017-10-15 18:15:35 +0300

    Updated docs

--- src/main/scala/enron/EnronAnalysis.scala
+++ src/main/scala/enron/EnronAnalysis.scala
@@ -52,6 +52,7 @@ object EnronAnalysis {
   val enronRdd: RDD[EnronEmail] = EnronData.readEnronEmail(spark)
 
   /** Returns the number of emails sent by each user
+    * Sorted by the number of emails per user in the descending order
    *  Hint1: consider using method `reduceByKey` on RDD[T].
    *  Hint2: consider using method `sortBy`
    */
@@ -67,24 +68,27 @@ object EnronAnalysis {
     ???
   }
 
-  /* Compute an inverted index of the set of articles, mapping each keyword
-   * to the Enron email in which it occurs.
+  /** Compute an inverted index of the set of emails, mapping each keyword
+   * to the Enron emails in which it occurs.
    */
   def makeInvertedIndex(keywords: List[String], rdd: RDD[EnronEmail]): RDD[(String, Iterable[EnronEmail])] = {
     ???
   }
 
+  /** Helpful method, which can be reused between makeInvertedIndex and rankKeywordsUsingIndex
+   * Consider caching the results using `cache`
+   */
   private def reverseIndex(rdd: RDD[EnronEmail], keywords: List[String]): RDD[(String, EnronEmail)] = {
     ???
   }
 
-  /* Compute the keywords ranking using the inverted index.
+  /** Compute the keywords ranking using the inverted index.
      */
   def rankKeywordsUsingIndex(index: RDD[(String, Iterable[EnronEmail])]): List[(String, Int)] = {
     ???
   }
 
-  /* Use `reduceByKey` so that the computation of the index and the ranking are combined.
+  /** Use `reduceByKey` so that the computation of the index and the ranking are combined.
    */
   def rankKeywordsReduceByKey(keywords: List[String], rdd: RDD[EnronEmail]): List[(String, Int)] = {
     ???
@@ -103,7 +107,7 @@ object EnronAnalysis {
     topicRanked1.take(10).foreach(println)
 
     /* Keywords ranked with the use of RecudeByKey */
-    val topicRanked2: List[(String, Int)] = timed("Ranking using inverted inxed and reduceByKey", rankKeywordsReduceByKey(topic1, enronRdd))
+    val topicRanked2: List[(String, Int)] = timed("Ranking using inverted index and reduceByKey", rankKeywordsReduceByKey(topic1, enronRdd))
     topicRanked2.take(10).foreach(println)
 
     /* Output the speed of each ranking */
